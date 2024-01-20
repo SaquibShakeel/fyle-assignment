534edbece4f8fa0b78a25bff2a88da80090a5fe9
SaquibShakeel <saquib.shakeel.1866@gmail.com>
2024-01-20 23:48:44 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## Fyle Web Development Internship - Github Repository Viewer
+
+This project is a web application designed to display the public Github repositories belonging to any specific user. The main goal is to create a user-friendly interface that retrieves and displays repository information from the Github API.
+
+# Requirements
+
+1. **API Documentation**: The application uses the Github REST API to fetch repository data.
+2. **Repository Topics Representation**: The application represents the topic of a particular repository. Keep in mind that one repository could have multiple topics.
+3. **Server-side Pagination**: Pagination is implemented on the server side to optimize performance. By default, the application displays 10 repositories per page, and the user can choose a maximum of 100 repositories per page.
+4. **Loaders for API Calls**: To enhance the user experience, loaders are displayed while API calls are in progress.
+5. **Optional**: The application optionally provides a search bar to allow users to search any valid github profile.
+
+# Technologies Used
+
+1. HTML, CSS, JavaScript: The core technologies for building the web application.
+2. Bootstrap: Used for styling and responsive design.
+
+## Getting Started
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/your-username/github-repo-viewer.git
+```
+2. Open the index.html file in your preferred web browser.
+2. Explore the Github repositories by entering a valid Github username.
+3. Use the pagination controls to navigate through the repositories.
+
+## Deployement
+
+Hosted at 
+
--- script.js
+++ script.js
@@ -1,50 +1,152 @@
-const username = "johnpapa";
+let username = "johnpapa";
 const apiUrl = `https://api.github.com/users/`;
 let perPage = 10;
 let currentPage = 1;
 let totalRepos = 0;
+let windowStart = 1;
+
+const searchButton = document.getElementById("search-button");
+
+const input = document.getElementById("username");
+
+const loader = document.getElementById("loader");
+
+input.addEventListener("input", ()=> {
+  document.querySelector("#form-error").textContent = "";
+  document.querySelector("#error").textContent = "";
+})
+
+searchButton.addEventListener("click", async (event) => {
+  event.preventDefault();
+  currentPage = 1;
+  try {
+    const inputUser = document.querySelector("#username").value;
+    if (inputUser.trim().length !== 0) {
+      username = inputUser;
+      await fetchProfileData();
+      await fetchRepoData();
+    } else {
+      throw new Error("Input field must not be empty");
+    }
+  } catch (error) {
+    console.log("error", error);
+    document.querySelector("#form-error").textContent = error.message;
+  } finally {
+    document.querySelector("#username").value = "";
+  }
+});
 
 const createPagination = () => {
-  const page = Math.ceil(totalRepos/perPage);
+  let page = Math.ceil(totalRepos / perPage);
   const pagination = document.getElementById("pagination-id");
   pagination.innerHTML = null;
 
-  for(let i = perPage*(currentPage-1); i < totalRepos && i < perPage*currentPage; i++) {
-    
+  const previousButton = document.createElement("li");
+  previousButton.className = `page-item ${currentPage == 1 && "disabled"}`;
+  const linkButtonPrev = document.createElement("span");
+  linkButtonPrev.className = `page-link`;
+  linkButtonPrev.textContent = "Previous";
+  previousButton.addEventListener("click", function () {
+    if (currentPage > 1) {
+      if (windowStart > 10 && currentPage%10 === 1) {
+        windowStart -= 10;
+      }
+      currentPage--;
+      fetchRepoData();
+      createPagination();
+    }
+  });
+  previousButton.appendChild(linkButtonPrev);
+  pagination.appendChild(previousButton);
+
+  for (let i = windowStart; i <= page && i < windowStart + 10; i++) {
+    const li = document.createElement("li");
+    li.className = `page-item ${i == currentPage && "active"}`;
+    const span = document.createElement("a");
+    span.href = "#";
+    span.className = `page-link`;
+    span.textContent = i;
+
+    li.addEventListener("click", async (event) => {
+      event.preventDefault();
+      currentPage = i;
+      await fetchRepoData();
+      await createPagination();
+    });
+
+    li.appendChild(span);
+    pagination.appendChild(li);
   }
+
+  const nextButton = document.createElement("li");
+  nextButton.className = `page-item ${currentPage == page && "disabled"}`;
+  const linkButtonNext = document.createElement("a");
+  linkButtonNext.href = "#";
+  linkButtonNext.className = `page-link`;
+  linkButtonNext.textContent = "Next";
+  nextButton.addEventListener("click", function (e) {
+    if (currentPage < page) {
+      if(currentPage%10 === 0 && windowStart + 10 < page) {
+        windowStart += 10;
+      }
+      currentPage++;
+      fetchRepoData();
+      createPagination();
+    }
+  });
+  nextButton.appendChild(linkButtonNext);
+  pagination.appendChild(nextButton);
 };
 
-const fetchProfileData = () => {
-  fetch(`${apiUrl}${username}`)
+const fetchProfileData = async () => {
+
+  loader.style.display = "block";
+  await fetch(`${apiUrl}${username}`)
     .then((response) => response.json())
     .then((data) => {
+
+      if(data?.message === "Not Found") {
+        throw new Error("User Not Found");
+      }
       totalRepos = data.public_repos;
       const profileDiv = document.getElementById("profile");
 
       profileDiv.innerHTML = `<img class="profile-image" src="${data?.avatar_url}" alt="profile_picture">
                                     <div class="about">
                                         <h1>${data?.name}</h1>
-                                        <p>${data?.bio}</p>
-                                        <div>${data?.location}</div>
+                                        <p class='bio'>${data?.bio}</p>
+                                        <p><strong>Location: </strong> ${data?.location}</p>
+                                        <a href="https://github.com/${username}">Profile Link</a>
                                         <a href="${data?.blog}">Website Link</a>
                                     </div>`;
+      loader.style.display = "none";
+      createPagination();
     })
-    .catch((error) => console.log("Error fetching profile data", error));
+    .catch((error) => {
+      document.querySelector("#error").textContent = error.message;
+      loader.style.display = "none";
+    });
 };
 
 const fetchRepoData = () => {
   fetch(`${apiUrl}${username}/repos?per_page=${perPage}&page=${currentPage}`)
     .then((response) => response.json())
     .then((repositories) => {
+
+      if(repositories?.message === "Not Found") {
+        throw new Error("Not Found");
+      }
       const repositoriesList = document.getElementById("repositories-list");
       repositoriesList.innerHTML = null;
 
       repositories.forEach((repo) => {
         const topicsList = document.createElement("div");
+        topicsList.className = `topic-list`;
         if (repo.topics.length !== 0) {
           repo.topics.forEach((topic) => {
             const topicDiv = document.createElement("div");
-            topicDiv.innerHTML = `${topic}`;
+            topicDiv.className = `topic-element`;
+            topicDiv.textContent = `${topic}`;
             topicsList.appendChild(topicDiv);
           });
         }
@@ -53,29 +155,29 @@ const fetchRepoData = () => {
         listItem.className = "repo-element";
         listItem.innerHTML = `<strong><a href="${
           repo.html_url
-        }" target="_blank">${repo.name}</a></strong><br><p>${
+        }" target="_blank">${repo.name}</a></strong><p>${
           repo.description || "No description available."
-        }</p><br>${topicsList.innerHTML || "No topics available"}`;
+        }</p><br>${
+          repo.topics.length !== 0
+            ? topicsList.outerHTML
+            : "<p>No topics available</p>"
+        }`;
         repositoriesList.appendChild(listItem);
+        document.querySelector("#error").textContent = "";
       });
     })
-    .catch((error) =>
-      console.error("Error fetching GitHub repositories:", error)
-    );
+    .catch((error) => {
+      if(document.querySelector("#error").textContent === "")
+        document.querySelector("#error").textContent = error.message;
+      console.log(error);
+    });
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-  try {
-    fetchProfileData();
-    fetchRepoData();
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 const selectPerPage = document.getElementById("per-page-cap");
 
-selectPerPage.addEventListener("change", (e) => {
+selectPerPage.addEventListener("change", async (e) => {
   perPage = e.target.value;
-  fetchRepoData();
+  currentPage = 1;
+  await fetchRepoData();
+  await createPagination();
 });
